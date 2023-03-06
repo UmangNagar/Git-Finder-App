556e2307846f0480e718bdf0105dbf00d98c949b
umang nagar <100286019+UmangNagar@users.noreply.github.com>
2023-03-06 15:39:45 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="card footer">
       <h2>GitFind | Developed by Umang Nagar | &copy; 2022</h2>
-      <a href="https://github.com/aayusharyan/gitfind" target="_blank" rel="noreferrer">Github Source Code</a>
+      <a href="https://github.com/UmangNagar/Git-Finder-App" target="_blank" rel="noreferrer">Github Source Code</a>
       <p> Made with ❤️ &amp; 💻</p>
     </div>
   )
